ce9b8f02c470f30673bf4dfff5ccc4e11fc99728
edinaldoFelipe <edinaldosantyago@hotmail.com>
2021-12-17 12:00:36 -0300

    Initial Commit

---
+++ MagniFinanceCollege/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MagniFinanceCollege.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
