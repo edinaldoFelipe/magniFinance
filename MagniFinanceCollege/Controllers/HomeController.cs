ef1ca6709d6d143991a62936276aa571d8a0868e
edinaldoFelipe <edinaldosantyago@hotmail.com>
2021-12-23 19:37:42 -0300

    Create backend courses

--- MagniFinanceCollege/Controllers/HomeController.cs
+++ MagniFinanceCollege/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using MagniFinanceCollege.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using MagniFinanceCollege.Models;
 
 namespace MagniFinanceCollege.Controllers
 {
@@ -19,11 +15,6 @@ namespace MagniFinanceCollege.Controllers
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
