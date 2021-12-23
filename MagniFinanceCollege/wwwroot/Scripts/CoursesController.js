92797bfdd8faa1e23abc25ebd3a46933cc0f15f9
edinaldoFelipe <edinaldosantyago@hotmail.com>
2021-12-23 19:36:49 -0300

    Create Courses frontend

---
+++ MagniFinanceCollege/wwwroot/Scripts/CoursesController.js
@@ -0,0 +1,102 @@
+﻿/**
+ *
+ * Controller to update attributes anjularJS to Course Pages
+ * and Request data by SignalR
+ *
+ */
+
+import app from "./app.js"
+import { generateBody } from "./../js/generateTable.js"
+
+
+/**
+ * Controller to Index Courses Page
+ * Begin when load page
+ */
+app.controller('CoursesController', ['$scope', 'signalRService', '$sce', function ($scope, signalRService, $sce) {
+    $scope.drop = false;
+    signalRService.connect($scope, "/coursesHub", "responseCourses");
+    $scope.$on('responseService', (response, data) => callBackSignalRResponseIndex($scope, $sce, data[0]));
+    signalRService.request("Index");
+    actionDeleteCourseBtn($scope, signalRService);
+}]);
+
+
+/**
+ * Controller to Create and Update Course Page
+ * Begin when load page
+ */
+app.controller('CoursesCreate', ['$scope', 'signalRService', function ($scope, signalRService) {
+    signalRService.connect($scope, "/coursesHub", "responseCourses");
+    $scope.$on('responseService', (response, data) => callBacksignalRResponseCreate(data[0]));
+    actionCreateCourseBtn(signalRService);
+}]);
+
+/**
+ * Clear Attributes
+ * @param {CoursesController} $scope
+ */
+function clearAttributes($scope) {
+    $scope.table = "";
+    $scope.$apply();
+}
+
+/**
+ * Callback SignalR
+ * Update Attributes
+ * @param {CoursesController} $scope
+ * @param {scope} $sce
+ * @param {object} data
+ */
+function callBackSignalRResponseIndex($scope, $sce, data) {
+    if ($scope.drop) {
+        if (data)
+            alertSuccess('Course was deleted')
+        else
+            alertError('Course was not delete')
+    }
+
+    $scope.drop = false;
+
+    clearAttributes($scope);
+    $scope.table = $sce.trustAsHtml(generateBody(data));
+    $scope.$apply();
+}
+
+/**
+ * Callback SignalR
+ * Redirect Page
+ * @param {bool} success
+ */
+function callBacksignalRResponseCreate(success) {
+    if (success)
+        alertSuccess(),
+        setTimeout(() => location = '/courses', 3000)
+    else
+        alertError()
+}
+
+/**
+ * Delete Course
+ * @param {CoursesController} $scope
+ * @param {signalR} signalRService
+ */
+function actionDeleteCourseBtn($scope, signalRService ) {
+    $('#confirmModel').on('click', function () {
+        const id = $(this).data('id');
+        $scope.drop = true;
+        signalRService.request("Destroy");
+    })
+}
+
+/**
+ * Action Button Save Course
+ * @param {signalR} signalRService
+ */
+function actionCreateCourseBtn(signalRService) {
+    $("#btnSaveCourse").on('click', () => {
+        const name = $("#nameCourse").val();
+        if (name)
+            signalRService.request("Store", name);
+    })
+}
---
+++ MagniFinanceCollege/wwwroot/js/generateTable.js
@@ -0,0 +1,67 @@
+﻿/**
+ * Render Rows to Default Listing Table
+ * @param {object} data
+ * @return string
+ */
+export const generateBody = data => {
+    let rows = "";
+
+    for (const row of data)
+        rows += generateRow(row)
+
+    return rows;
+}
+
+/**
+ * Render cells of row
+ * @param {object} row
+ * @return string
+ */
+const generateRow = row => {
+    let cells = "";
+
+    for (const cell of Object.values(row))
+        cells += `<td>${cell}</td>`;
+
+    return `<tr>${cells}<td class="p-1 align-middle text-right">
+        ${getHTMLBtnEdit(row.id)} ${getHTMLBtnDelete(row.id)}</td></tr>`;
+}
+
+const getHTMLBtnEdit = id => {
+    return `<button onclick="location.href='/Courses/Update/${id}'"
+            class="btn btn-outline-primary p-1 px-2 mr-0">
+            <i class="far fa-edit m-0"></i>
+        </button>`;
+}
+
+const getHTMLBtnDelete = id => {
+    return `<button data-toggle="modal" data-target="#myModal"
+            class="btn btn-outline-danger p-1 px-2" data-id="${id}">
+            <i class="feather icon-trash-2 m-0"></i>
+        </button>`;
+}
+
+/**
+ * Render Rows to Default Listing Table Activities
+ * @param {data} data
+ */
+export const generateActivities = data => {
+    let rows = "";
+
+    for (const row of data)
+        rows += generateRowActivities(row)
+
+    return rows;
+}
+
+function generateRowActivities(row) {
+    return `<tr><td><img class="rounded-circle" style="width:40px;" src="/theme/assets/images/user/${row.img}.jpg"></td>
+            <td>
+                <h6 class="mb-1">${row.name}</h6>
+                <p class="m-0">${row.description}</p>
+            </td>
+            <td>
+                <h6 class="text-muted"><i class="fas fa-circle text-c-${row.status == "active" ? "green" : "red"} f-10 m-r-15"></i>${row.date}</h6>
+            </td>
+        </tr>`;
+}
